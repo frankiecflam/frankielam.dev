32c482ec2f5e0ad7aac02fab35e5c8909a824733
Frankie Lam <frankie191193@gmail.com>
2023-02-09 12:01:21 +0000

    config for custom black and white color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,7 +8,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        black: {
+          DEFAULT: "#111010",
+          100: "#211F1F",
+          200: "#333030",
+          300: "#454141",
+          400: "#565151",
+          500: "#686262",
+        },
+        while: {
+          DEFAULT: "#FFFFFF",
+          100: "#EDEDED",
+          200: "#DCDCDC",
+          300: "#BABABA",
+          400: "#A9A9A9",
+          500: "#989898",
+        },
+      },
+    },
   },
   plugins: [],
 };
