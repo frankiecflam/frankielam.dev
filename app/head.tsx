414d0aa771749d4d392b2ca1a1a32e72d460e422
Frankie Lam <frankie191193@gmail.com>
2023-02-15 18:17:30 +0000

    update og tags

--- app/head.tsx
+++ app/head.tsx
@@ -8,11 +8,28 @@ export default function Head() {
         content="Hi, I am Frankie. I am a self-taught developer with more than 1 year of project experience in front-end development. I am passionate about building things that live on the internet through curiosity and perseverance."
       />
       <link rel="icon" href="/favicon.ico" />
+      {/* OG Tags */}
       <meta property="og:title" content="Frankie Lam - Frontend Developer" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="www.frankielam.dev" />
+      <meta property="og:url" content="https://www.frankielam.dev" />
       <meta
         property="og:image"
+        content="https://www.frankielam.dev/api/og?title=Frankie%20Lam&description=self-taught%20frontend%20developer"
+      />
+      {/* Twitter OG Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:domain"
+        content="frankielamdev-git-optimisation-og-tags-frankiecflam.vercel.app"
+      />
+      <meta property="twitter:url" content="https://www.frankielam.dev" />
+      <meta name="twitter:title" content="Frankie Lam - Frontend Developer" />
+      <meta
+        name="twitter:description"
+        content="Hi, I am Frankie. I am a self-taught developer with more than 1 year of project experience in front-end development. I am passionate about building things that live on the internet through curiosity and perseverance."
+      />
+      <meta
+        name="twitter:image"
         content="https://www.frankielam.dev/api/og?title=Frankie%20Lam&description=self-taught%20frontend%20developer"
       />
     </>
